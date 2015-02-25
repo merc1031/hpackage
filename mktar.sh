814b08c5e6e4d387480c72f33f37957974680a78
Leonidas Loucas <lloucas@imvu.com>
2015-02-25 10:17:03 -0800

    fixings

--- mktar.sh
+++ mktar.sh
@@ -2,6 +2,9 @@
 
 PROJNAME=$(debian/haskell-extract-changelog-name-version.sh name)
 VERSION=$(debian/haskell-extract-changelog-name-version.sh version)
-rsync -avP --exclude=".cabal-sandbox" --exclude="cabal.sandbox.config" --exclude="dist" --exclude=".git" $PROJNAME $PROJNAME-$VERSION
+
+rm -rf $PROJNAME-$VERSION
+rm -f $PROJNAME-$VERSION.tar.gz
+
+rsync -avP --exclude=".cabal-sandbox" --exclude="cabal.sandbox.config" --exclude="dist" --exclude=".git" $PROJNAME/ $PROJNAME-$VERSION
 tar czvf $PROJNAME-$VERSION.tar.gz $PROJNAME-$VERSION
-#maybe cabal sdist ... IS idiotic
